4bdb4261cd3aefbe6e8a3fbf1f9194a0719c8ab9
AK Singh <167464412+aksingh4545@users.noreply.github.com>
2026-04-29 18:10:18 +0530

    Change DATABASE_URL to use os.getenv
    Updated DATABASE_URL to use environment variable.

--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres:1234@localhost:5432/document_intelligence_db"
+os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
